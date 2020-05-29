0455c24998b7f6aa56d2747ab718f75d8b87d0fb
matejcsok-istvan <matejcsok.upwork@gmail.com>
2020-05-29 15:18:24 +0200

    comment policy page added

---
+++ pages/policy.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+const PolicyPage: NextPage<{}> = () => (
+  <div>
+    <h1>Policy:</h1>
+    <ul>
+      <li>be nice</li>
+      <li>do not be rude</li>
+    </ul>
+  </div>
+);
+
+export default PolicyPage;
